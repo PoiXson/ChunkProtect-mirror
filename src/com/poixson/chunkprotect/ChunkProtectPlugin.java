264371ef6670fd4e46a30727de42d1bef5574db2
lorenzop <lorenzo@poixson.com>
2023-01-17 12:15:29 -0500

    added main plugin class - unfinished

---
+++ src/com/poixson/chunkprotect/ChunkProtectPlugin.java
@@ -0,0 +1,29 @@
+package com.poixson.chunkprotect;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public class ChunkProtectPlugin extends JavaPlugin {
+
+
+
+	public ChunkProtectPlugin() {
+		super();
+	}
+
+
+
+	@Override
+	public void onEnable() {
+	}
+
+
+
+	@Override
+	public void onDisable() {
+	}
+
+
+
+
+}
