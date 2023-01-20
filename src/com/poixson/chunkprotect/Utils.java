ae7c0f78373df37ea2cc10ebb78e040887c5ca4e
lorenzop <lorenzo@poixson.com>
2023-01-19 20:05:56 -0500

    added EqualsPlayer() and EqualsUUID() functions to Utils class

--- src/com/poixson/chunkprotect/Utils.java
+++ src/com/poixson/chunkprotect/Utils.java
@@ -1,14 +1,28 @@
 package com.poixson.chunkprotect;
 
+import java.util.UUID;
+
+import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffect;
 
 
-public class Utils {
+public final class Utils {
 	private Utils() {}
 
 
 
-	public static boolean PotionEffectEquals(final PotionEffect effectA, final PotionEffect effectB) {
+	public static boolean EqualsPlayer(final Player playerA, final Player playerB) {
+		if (playerA == null || playerB == null)
+			return (playerA == null && playerB == null);
+		return EqualsUUID(playerA.getUniqueId(), playerB.getUniqueId());
+	}
+	public static boolean EqualsUUID(final UUID uuidA, final UUID uuidB) {
+		if (uuidA == null || uuidB == null) {
+			return (uuidA == null && uuidB == null);
+		}
+		return uuidA.equals(uuidB);
+	}
+	public static boolean EqualsPotionEffect(final PotionEffect effectA, final PotionEffect effectB) {
 		if (effectA == null || effectB == null) {
 			return (effectA == null && effectB == null);
 		}
