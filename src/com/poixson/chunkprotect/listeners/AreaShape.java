8efd11ca33cc0a17a11c57693167e9990ef04ceb
lorenzop <lorenzo@poixson.com>
2023-01-19 20:05:09 -0500

    added AreaShape enum

---
+++ src/com/poixson/chunkprotect/listeners/AreaShape.java
@@ -0,0 +1,37 @@
+package com.poixson.chunkprotect.listeners;
+
+
+public enum AreaShape {
+
+	CIRCLE,
+	SQUARE;
+
+
+
+	public static AreaShape Get(final String str) {
+		if (str == null)
+			return null;
+		final String lower = str.toLowerCase();
+		switch (lower) {
+		case "circle": return CIRCLE;
+		case "square": return SQUARE;
+		}
+		if (lower.startsWith("c")) return CIRCLE;
+		if (lower.startsWith("s")) return SQUARE;
+		return null;
+	}
+
+
+
+	@Override
+	public String toString() {
+		switch(this) {
+		case CIRCLE: return "circle";
+		case SQUARE: return "square";
+		}
+		return null;
+	}
+
+
+
+}
