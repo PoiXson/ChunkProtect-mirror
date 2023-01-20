673c4132eb36d3c3ae7a39def9209747aab0d029
lorenzop <lorenzo@poixson.com>
2023-01-19 20:04:24 -0500

    added config options for area shape and protect area tier

--- src/com/poixson/chunkprotect/ChunkProtectPlugin.java
+++ src/com/poixson/chunkprotect/ChunkProtectPlugin.java
@@ -4,6 +4,7 @@ import java.util.concurrent.atomic.AtomicReference;
 import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.poixson.chunkprotect.listeners.AreaShape;
 import com.poixson.chunkprotect.listeners.BeaconHandler;
 import com.poixson.chunkprotect.listeners.BeaconListener;
 import com.poixson.chunkprotect.listeners.BlockPlaceBreakListener;
@@ -11,9 +12,17 @@ import com.poixson.chunkprotect.listeners.BlockPlaceBreakListener;
 
 public class ChunkProtectPlugin extends JavaPlugin {
 
+	public static final AreaShape DEFAULT_AREA_SHAPE = AreaShape.CIRCLE;
 	public static final int DEFAULT_SPAWN_RADIUS = 56;
+	public static final int DEFAULT_PROTECTED_RADIUS_TIER1 = 1;
+	public static final int DEFAULT_PROTECTED_RADIUS_TIER2 = 2;
+	public static final int DEFAULT_PROTECTED_RADIUS_TIER3 = 5;
+	public static final int DEFAULT_PROTECTED_RADIUS_TIER4 = 9;
 
-	protected final AtomicReference<FileConfiguration> config = new AtomicReference<FileConfiguration>(null);
+	// configs
+	protected final AtomicReference<FileConfiguration> config     = new AtomicReference<FileConfiguration>(null);
+	protected final AtomicReference<AreaShape>         areaShape  = new AtomicReference<AreaShape>(null);
+	protected final AtomicReference<Integer[]>         areaSizes  = new AtomicReference<Integer[]>(null);
 
 	// listeners
 	protected final AtomicReference<BeaconHandler>  beaconHandler  = new AtomicReference<BeaconHandler>(null);
@@ -111,6 +120,28 @@ public class ChunkProtectPlugin extends JavaPlugin {
 			this.configDefaults(cfg);
 			cfg.options().copyDefaults(true);
 			super.saveConfig();
+			// area shape
+			{
+				final AreaShape shape = AreaShape.Get(cfg.getString("Area Shape"));
+				if (shape == null) {
+					log.warning("Area shape not set or invalid! Using default: " + DEFAULT_AREA_SHAPE.toString());
+					this.areaShape.set(DEFAULT_AREA_SHAPE);
+				} else {
+					this.areaShape.set(shape);
+				}
+			}
+			// protected area radius
+			{
+				final Integer[] sizes = new Integer[] {
+					Integer.valueOf(0),
+					Integer.valueOf( cfg.getInt("Protect Area Tier 1") ),
+					Integer.valueOf( cfg.getInt("Protect Area Tier 2") ),
+					Integer.valueOf( cfg.getInt("Protect Area Tier 3") ),
+					Integer.valueOf( cfg.getInt("Protect Area Tier 4") ),
+				};
+				this.areaSizes.set(sizes);
+			}
+		}
 		}
 	}
 	protected void saveConfigs() {
@@ -118,7 +149,28 @@ public class ChunkProtectPlugin extends JavaPlugin {
 		super.saveConfig();
 	}
 	protected void configDefaults(final FileConfiguration cfg) {
+		cfg.addDefault("Area Shape", DEFAULT_AREA_SHAPE.toString());
 		cfg.addDefault("Spawn Radius", Integer.valueOf(DEFAULT_SPAWN_RADIUS));
+		cfg.addDefault("Protect Area Tier 1", Integer.valueOf(DEFAULT_PROTECTED_RADIUS_TIER1));
+		cfg.addDefault("Protect Area Tier 2", Integer.valueOf(DEFAULT_PROTECTED_RADIUS_TIER2));
+		cfg.addDefault("Protect Area Tier 3", Integer.valueOf(DEFAULT_PROTECTED_RADIUS_TIER3));
+		cfg.addDefault("Protect Area Tier 4", Integer.valueOf(DEFAULT_PROTECTED_RADIUS_TIER4));
+	}
+
+
+
+	public AreaShape getAreaShape() {
+		return this.areaShape.get();
+	}
+
+	public int getProtectedAreaRadius(final int tier) {
+		if (tier < 0 || tier > 4) throw new RuntimeException("Tier value is out of range: " + Integer.toString(tier));
+		final Integer[] sizes = this.areaSizes.get();
+		if (sizes == null) {
+			log.warning(LOG_PREFIX + "protected area sizes not set!");
+			return 0;
+		}
+		return sizes[tier].intValue();
 	}
 
 
